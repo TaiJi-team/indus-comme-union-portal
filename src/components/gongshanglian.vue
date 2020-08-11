24bdf1adea6962a21bb927ce7e1ef25379384ea9
shiwenjun <18134469613@163.com>
2020-08-11 17:49:37 +0800

    工商联第一版提交

---
+++ src/components/gongshanglian.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="gongshanglian">
+    <el-container>
+    <el-header>
+      <div class="demo-image__placeholder">
+        <div class="block">
+          <span class="demonstration">默认</span>
+          <el-image src="./assets/logo_1.png"></el-image>
+        </div>
+      </div>
+    </el-header>
+    <el-main>Main</el-main>
+  </el-container>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "gongshanglian"
+    }
+</script>
+
+<style scoped>
+
+</style>
